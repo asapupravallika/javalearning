e6022bd64081e8763243b1b862401d6d4287146e
asapupravallika <asapupravallika@gmail.com>
2026-06-06 16:45:05 +0530

    feat: Checking Age Group

---
+++ src/com/codegnan/fundamentals/CheckingAgeGroup.java
@@ -0,0 +1,13 @@
+package com.codegnan.fundamentals;
+
+import java.util.Scanner;
+
+public class CheckingAgeGroup {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int age = sc.nextInt();
+		sc.close();
+	}
+
+}
